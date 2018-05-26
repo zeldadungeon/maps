d50d9fa9d9c86ce32c5028c3fae6f4e6435aacf1
LockeExile <lockeexile@gmail.com>
2018-05-25 19:29:05 -0700

    refactor popup

--- src/common/Marker.ts
+++ src/common/Marker.ts
@@ -1,8 +1,8 @@
-import * as $ from "jquery";
 import * as L from "leaflet";
 import * as Schema from "JSONSchema";
 import { Layer } from "Layer";
 import { MarkerContainer } from "MarkerContainer";
+import { Popup } from "common/Popup";
 
 export class Marker extends L.Marker {
     public id: string;
@@ -26,77 +26,38 @@ export class Marker extends L.Marker {
         const marker = new Marker(json.id, json.name, json.coords, icon);
         marker.layer = layer;
         const linkParts = json.link ? json.link.split("#") : [];
+        const editLink = `${
+            layer.infoSource === "summary" || layer.infoSource === "section" || !layer.infoSource ? linkParts[0] :
+            layer.infoSource === "subpage" && linkParts[1] ? `${linkParts[0]}/Map/${linkParts[1]}` :
+            layer.infoSource === "subpage" ? `${linkParts[0]}/Map` : linkParts[0]
+        }`;
 
         if (layer.icon) {
-            const container = L.DomUtil.create("div", "zd-popup");
-            const popup = L.popup({
-                autoPan: true,
-                maxWidth: 300
-            }).setContent(container);
-            marker.bindPopup(popup);
-
-            const title = L.DomUtil.create("h3", "zd-popup__title", container);
-            if (json.link) {
-                const link = L.DomUtil.create("a", "", title);
-                link.setAttribute("target", "_blank");
-                link.setAttribute("href", `/wiki/${encodeURIComponent(json.link)}`);
-                link.innerText = json.name;
-            } else {
-                title.innerText = json.name;
-            }
-
-            const body = L.DomUtil.create("div", "zd-popup__body", container);
-            if (layer.infoSource) {
-                const loading = L.DomUtil.create("div", "", body);
-                loading.style.textAlign = "center";
-                L.DomUtil.create("i", "fa fa-circle-o-notch fa-spin fa-3x fa-fw", loading);
-
-                marker.on("popupopen", () => {
-                    const contentPromise =
-                        layer.infoSource === "summary" ? Marker.getContentFromSummary(linkParts[0]) :
-                        layer.infoSource === "section" ? Marker.getContentFromSection(
-                            linkParts[0],
-                            json.id.match(/^Seed\d{3}$/) ? `${json.id}summary` : linkParts[1] || "summary") :
-                        layer.infoSource === "subpage" ? Marker.getContentFromSubpage(linkParts[0], linkParts[1]) :
-                        Marker.getContentFromPage(layer.infoSource);
-                    contentPromise.then(content => {
-                        body.innerHTML = content;
-                        // TODO wire up internal links
-                        // $('.internal-link').click(function(event) {
-                        //     center({
-                        //       target: $(event.target).data('target'),
-                        //       lat: $(event.target).data('lat'),
-                        //       lng: $(event.target).data('lng')
-                        //     });
-                        //   });
-                        popup.setContent(container); // force it to resize and recenter
-                    });
-                });
-            }
-
-            const controls = L.DomUtil.create("div", "zd-popup__controls", container);
-            const completeButton = L.DomUtil.create("a", "zd-popup__control zd-popup__control--complete", controls);
-            L.DomUtil.create("i", "fa fa-check", completeButton).title = "Mark completed";
-            const uncompleteButton = L.DomUtil.create("a", "zd-popup__control zd-popup__control--uncomplete", controls);
-            L.DomUtil.create("i", "fa fa-undo", uncompleteButton).title = "Mark not completed";
-            const editButton = L.DomUtil.create("a", "zd-popup__control zd-popup__control--edit", controls);
-            editButton.setAttribute("target", "_blank");
-            editButton.setAttribute("href", `/wiki/index.php?action=edit&title=${encodeURIComponent(
-                layer.infoSource === "summary" || layer.infoSource === "section" || !layer.infoSource ? linkParts[0] :
-                layer.infoSource === "subpage" && linkParts[1] ? `${linkParts[0]}/Map/${linkParts[1]}` :
-                layer.infoSource === "subpage" ? `${linkParts[0]}/Map` : linkParts[0]
-            )}`);
-            L.DomUtil.create("i", "fa fa-edit", editButton).title = "Edit";
-            const linkButton = L.DomUtil.create("a", "zd-popup__control zd-popup__control--permalink", controls);
-            linkButton.setAttribute("href", `?id=${json.id}`);
-            L.DomUtil.create("i", "fa fa-link", linkButton).title = "Permalink";
-
-            // TODO
-            L.DomEvent.addListener(completeButton, "click", () => {
-                console.log(`Completed ${json.name}`);
+            const popup = Popup.create({
+                id: json.id,
+                name: json.name,
+                link: json.link,
+                editLink: editLink,
+                complete: () => {
+                    console.log(`Completed ${json.name}`);
+                },
+                uncomplete: () => {
+                    console.log(`Uncompleted ${json.name}`);
+                }
             });
-            L.DomEvent.addListener(uncompleteButton, "click", () => {
-                console.log(`Uncompleted ${json.name}`);
+            marker.bindPopup(popup);
+            marker.on("popupopen", () => {
+                if (layer.infoSource === "summary") {
+                    popup.loadContentFromSummary(linkParts[0]);
+                } else if (layer.infoSource === "section") {
+                    popup.loadContentFromSection(
+                        linkParts[0],
+                        json.id.match(/^Seed\d{3}$/) ? `${json.id}summary` : linkParts[1] || "summary");
+                } else if (layer.infoSource === "subpage") {
+                    popup.loadContentFromSubpage(linkParts[0], linkParts[1]);
+                } else if (layer.infoSource) {
+                    popup.loadContentFromPage(layer.infoSource);
+                }
             });
         } else {
             marker.bindTooltip(json.name, {
@@ -113,48 +74,6 @@ export class Marker extends L.Marker {
         }
 
         return marker;
-    }
-
-    private static getContentFromSummary(pageTitle: string) {
-        return $.getJSON(`https://www.zeldadungeon.net/wiki/api.php?format=json&action=query&prop=pageprops&titles=${encodeURIComponent(pageTitle)}&callback=?`)
-            .then(result => {
-                const pageId = Object.keys(result.query.pages)[0];
-
-                return pageId === "-1" ? "" : `<p>${result.query.pages[pageId].pageprops.description}</p>`;
-                // const description = L.DomUtil.create("p");
-                // const pageId = Object.keys(result.query.pages)[0];
-                // if (pageId !== "-1") {
-                //     description.innerText = result.query.pages[pageId].pageprops.description;
-                // }
-
-                // return description;
-            });
-    }
-
-    private static getContentFromSection(pageTitle: string, sectionName: string) {
-        return $.getJSON(`https://www.zeldadungeon.net/wiki/api.php?format=json&action=parse&prop=text&contentmodel=wikitext&text=%7b%7b%23lst:${encodeURIComponent(pageTitle)}|${encodeURIComponent(sectionName)}}}&callback=?`)
-            .then(result => {
-                let text = result.parse.text["*"];
-                text = text.replace(/\s*<!--[\s\S]*-->\s*/g, "");
-                if (text.match(/page does not exist/)) {
-                    text = text.replace(`>${name}</a>`, ">Create this article</a>");
-                }
-
-                return text;
-            });
-    }
-
-    private static getContentFromSubpage(pageTitle: string, subsubpage: string) {
-        const subpage = subsubpage ? `Map/${subsubpage}` : "Map";
-
-        return Marker.getContentFromPage(`${pageTitle}/${subpage}`);
-    }
-
-    private static getContentFromPage(pageTitle: string) {
-        return $.getJSON(`https://www.zeldadungeon.net/wiki/api.php?format=json&action=parse&page=${encodeURIComponent(pageTitle)}&callback=?`)
-            .then(result => {
-                return result.parse && result.parse.text["*"] || "";
-            });
     }
 
     public addToTileContainer(container: MarkerContainer): void {
---
+++ src/common/Popup.ts
@@ -0,0 +1,135 @@
+import * as $ from "jquery";
+import * as L from "leaflet";
+
+const API_URL = "https://www.zeldadungeon.net/wiki/api.php?format=json";
+
+enum ContentState {
+    Initial,
+    Loading,
+    Loaded
+}
+
+export interface Options extends L.PopupOptions {
+    id: string;
+    name: string;
+    link?: string;
+    editLink?: string;
+    complete(): void;
+    uncomplete(): void;
+}
+
+export class Popup extends L.Popup {
+    private container: HTMLElement;
+    private body: HTMLElement;
+    private contentState = ContentState.Initial;
+
+    private constructor(options: Options) {
+        super(options);
+
+        this.container = L.DomUtil.create("div", "zd-popup");
+
+        const title = L.DomUtil.create("h3", "zd-popup__title", this.container);
+        if (options.link) {
+            const link = L.DomUtil.create("a", "", title);
+            link.setAttribute("target", "_blank");
+            link.setAttribute("href", `/wiki/${encodeURIComponent(options.link)}`);
+            link.innerText = options.name;
+        } else {
+            title.innerText = options.name;
+        }
+
+        this.body = L.DomUtil.create("div", "zd-popup__body", this.container);
+
+        const controls = L.DomUtil.create("div", "zd-popup__controls", this.container);
+
+        const completeButton = L.DomUtil.create("a", "zd-popup__control zd-popup__control--complete", controls);
+        L.DomUtil.create("i", "fa fa-check", completeButton).title = "Mark completed";
+        L.DomEvent.addListener(completeButton, "click", options.complete);
+
+        const uncompleteButton = L.DomUtil.create("a", "zd-popup__control zd-popup__control--uncomplete", controls);
+        L.DomUtil.create("i", "fa fa-undo", uncompleteButton).title = "Mark not completed";
+        L.DomEvent.addListener(uncompleteButton, "click", options.uncomplete);
+
+        if (options.editLink) {
+            const editButton = L.DomUtil.create("a", "zd-popup__control zd-popup__control--edit", controls);
+            editButton.setAttribute("target", "_blank");
+            editButton.setAttribute("href", `/wiki/index.php?action=edit&title=${encodeURIComponent(options.editLink)}`);
+            L.DomUtil.create("i", "fa fa-edit", editButton).title = "Edit";
+        }
+
+        const linkButton = L.DomUtil.create("a", "zd-popup__control zd-popup__control--permalink", controls);
+        linkButton.setAttribute("href", `?id=${options.id}`);
+        L.DomUtil.create("i", "fa fa-link", linkButton).title = "Permalink";
+
+        this.setContent(this.container);
+    }
+
+    public static create(options: Options): Popup {
+        if (options.autoPan == undefined) { options.autoPan = true; }
+        if (options.maxWidth == undefined) { options.maxWidth = 300; }
+
+        return new Popup(options);
+    }
+
+    public loadContentFromSummary(pageTitle: string): void {
+        if (this.contentState === ContentState.Initial) {
+            this.startLoading();
+            $.getJSON(`${API_URL}&action=query&prop=pageprops&titles=${encodeURIComponent(pageTitle)}&callback=?`)
+                .then(result => {
+                    const pageId = Object.keys(result.query.pages)[0];
+                    this.loadContent(pageId === "-1" ? "" : `<p>${result.query.pages[pageId].pageprops.description}</p>`);
+                });
+        }
+    }
+
+    public loadContentFromSection(pageTitle: string, sectionName: string): void {
+        if (this.contentState === ContentState.Initial) {
+            this.startLoading();
+            const textToParse = encodeURIComponent(`{{#lst:${pageTitle}|${sectionName}}}`);
+            $.getJSON(`${API_URL}&action=parse&prop=text&contentmodel=wikitext&text=${textToParse}&callback=?`)
+                .then(result => {
+                    let content = result.parse.text["*"];
+                    content = content.replace(/\s*<!--[\s\S]*-->\s*/g, "");
+                    if (content.match(/page does not exist/)) {
+                        content = content.replace(`>${name}</a>`, ">Create this article</a>");
+                    }
+                    this.loadContent(content);
+                });
+        }
+    }
+
+    public loadContentFromSubpage(pageTitle: string, subsubpage: string): void {
+        const subpage = subsubpage ? `Map/${subsubpage}` : "Map";
+        this.loadContentFromPage(`${pageTitle}/${subpage}`);
+    }
+
+    public loadContentFromPage(pageTitle: string): void {
+        if (this.contentState === ContentState.Initial) {
+            this.startLoading();
+            $.getJSON(`${API_URL}&action=parse&page=${encodeURIComponent(pageTitle)}&callback=?`)
+                .then(result => {
+                    this.loadContent(result.parse && result.parse.text["*"] || "");
+                });
+        }
+    }
+
+    private startLoading(): void {
+        this.contentState = ContentState.Loading;
+        const loading = L.DomUtil.create("div", "zd-popup__loading-indicator", this.body);
+        L.DomUtil.create("i", "fa fa-circle-o-notch fa-spin fa-3x fa-fw", loading);
+    }
+
+    private loadContent(content: string): void {
+        this.body.innerHTML = content;
+        // TODO wire up internal links
+        // $('.internal-link').click(function(event) {
+        //     center({
+        //       target: $(event.target).data('target'),
+        //       lat: $(event.target).data('lat'),
+        //       lng: $(event.target).data('lng')
+        //     });
+        //   });
+        this.setContent(this.container); // force it to resize and recenter
+        this.contentState = ContentState.Loaded;
+    }
+}
