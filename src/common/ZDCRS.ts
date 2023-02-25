6b96d17ae685b9fc24d0e5d5dbb5d8b95c8e19e5
LockeExile <lockeexile@gmail.com>
2023-02-25 11:56:35 -0800

    fix build break due to L.CRS type missing transformation property

--- src/common/ZDCRS.ts
+++ src/common/ZDCRS.ts
@@ -3,8 +3,9 @@ import * as L from "leaflet";
 export function create(mapSize: number, tileSize: number): L.CRS {
   const scale = tileSize / mapSize;
   const offset = tileSize / 2;
-  const zdcrs = L.Util.extend({}, L.CRS.Simple);
-  zdcrs.transformation = new L.Transformation(scale, offset, -scale, offset);
+  const zdcrs = L.Util.extend({}, L.CRS.Simple, {
+    transformation: new L.Transformation(scale, offset, -scale, offset),
+  });
 
   return zdcrs;
 }
